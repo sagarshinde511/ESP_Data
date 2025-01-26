c0fd1ae1ace48774c93c8c6419769f6c894fa58f
sagarshinde511 <amplifierelectronics9@gmail.com>
2025-01-26 08:52:16 +0530

    Update esp_api.py

--- esp_api.py
+++ esp_api.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # Create sample data with latitude and longitude
 data = pd.DataFrame({
-    'lat': [17.2987556],
-    'lon': [74.1900642]
+    'lat': [17.2987556,17.29085],
+    'lon': [74.1900642,74.1842447]
 })
 
 # Display a map
